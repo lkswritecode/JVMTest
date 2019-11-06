fc6c50a7effe0430df4778c1b7deef2126e48ee4
test <1091343937@qq.com>
2019-11-06 18:26:40 +0800

    直接内存 02 使用Unsafe获取直接内存地址 检查直接内存是否被 GC-垃圾回收管理

--- Demo/src/directmemory/DirectMemory_02.java
+++ Demo/src/directmemory/DirectMemory_02.java
@@ -12,6 +12,10 @@ import java.lang.reflect.Field;
  * Unsafe不建议使用，此类为JVM使用的私有类
  * 构造方法为  private Unsafe()
  * 相关简书 https://www.jianshu.com/p/db8dce09232d
+ *
+ * ByteBuffer 底层就使用Unsafe对象完成直接内存的分配回收，并主动调用freeMemory方法
+ * ByteBuffer底层就是使用了Cleaner(虚引用)来监测ByteBuffer对象
+ * 一旦ByteBuffer对象被垃圾回收了那么就会由一个叫ReferenceHandler的线程通过freeMemory来释放直接内存
  */
 public class DirectMemory_02 {
      static int SIZE_1G = 1024*1024*1024;
